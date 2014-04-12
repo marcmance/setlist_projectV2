b5ebdf5b9730a223b02f8eb713498fc0e4bda243
marc <marc.mance@Gmail.com>
2014-04-12 01:43:14 -0400

    BE: Added Setlist Song Model. FE: Styling setlist page

--- api/v1/controllers/setlistController.php
+++ api/v1/controllers/setlistController.php
@@ -7,7 +7,24 @@
 		}
 
 		public function get($id) {
-			return json_encode($this->model->join("venue", array("venue_name", "city", "state"))->join("artist", array("artist_name"))->find($id));
+
+			//query for Setlist by Id
+			$results = $this->model->join("venue", array("venue_name", "city", "state"))->join("artist", array("artist_name"))->find($id);
+			
+			if(!empty($results)) {
+				
+				$ss = new Setlist_Song();
+			
+				//query all songs for Setlist
+				$results['songs'] = $ss->join("song", array("song_id","song_name"))
+					->join("album",array("album_id","album_name","cover_art_url"))
+					->where("setlist_id",$id)
+					->order("setlist_order", true)
+					->findAll();
+			}
+
+			//printArray($results);
+			return json_encode($results);
 		}
 	}
 ?>
