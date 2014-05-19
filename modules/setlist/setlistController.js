9a695454bb4758cc3a2b8be4804e361e7abeade9
marc <marc.mance@Gmail.com>
2014-05-18 23:52:53 -0400

    FE: Styled all setlist page

--- modules/setlist/setlistController.js
+++ modules/setlist/setlistController.js
@@ -4,7 +4,7 @@ mySetlist.controller('SetlistController', ['$scope', '$route', 'setlist',
 	    setlist.get($route.current.params.id).$promise.then(function(result){
 	        console.log(result);
 	        $scope.setlist = result;
-	        $scope.setlist.date = new Date($scope.setlist.date + " 03:00:00");
+	        //$scope.setlist.date = new Date($scope.setlist.date + " 03:00:00");
 	    }); 
 
 	    $scope.filters = { };
