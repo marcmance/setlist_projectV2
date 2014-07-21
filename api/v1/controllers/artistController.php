bbf33c9352fb4b95a45dc4483df876c59ca31407
marc <marc.mance@Gmail.com>
2014-07-21 01:24:12 -0400

    New Artist Page, added check on FE/BE for an artist already in the system

--- api/v1/controllers/artistController.php
+++ api/v1/controllers/artistController.php
@@ -23,5 +23,29 @@
 		public function index() {
 			return json_encode($this->model->order("artist_name",true)->findAll());
 		}
+
+		public function post() {
+
+			$json = json_decode(file_get_contents('php://input'));
+			
+			$this->model = new $this->model_name();
+			$sucesss = $this->model->setModelFields($json);
+
+			if($sucesss) {
+				$this->checkModel = new $this->model_name();
+				if($this->checkModel->where("artist_name", $this->model->artist_name)->findAll()) {
+					header('HTTP/1.1 409 User Already Exists');
+					$results['error_message'] = $this->model->artist_name . " aready exists in the system";
+				}
+				else {
+					$results['id'] = $this->model->insert();
+				}
+			}
+			else {
+				$results['error_message'] = "Wrong fields";
+			}
+			
+			echo json_encode($results);
+		}
 	}
 ?>
--- modules/artist/controllers/newArtistController.js
+++ modules/artist/controllers/newArtistController.js
@@ -1,12 +1,22 @@
 mySetlist.controller('NewArtistController', ['$scope', '$route', 'artist',  function ($scope, $route, artist) {
     
     $scope.artist = {
-        artist_name: ''
+        artist_name: '',
     };
 
+    $scope.showMsg = false;
+    $scope.message = '';
+
     $scope.insertArtist = function() {
-        artist.post($scope.artist).$promise.then(function(result){;
-        	console.log("what up doge? " + result);
+        artist.post($scope.artist).$promise.then(function(result){
+        	$scope.artist.artist_name = '';
+        	$scope.showMsg = true;
+        	$scope.message = 'Artist successfully posted!';
+    	},
+    	function(error) {
+    		$scope.showMsg = true;
+        	$scope.message = error.data.error_message;
+        	$scope.artist.artist_name = '';
     	});
     }
 
